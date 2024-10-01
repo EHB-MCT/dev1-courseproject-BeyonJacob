b4c0ab3b30a9ae6b73e04e9ea215cc4acbb51a98
Beyon Jacob <beyon.jacob@student.ehb.be>
2024-10-01 14:41:55 +0200

    My sign

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,20 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+squ1();
+function squ1()
+{
+context.beginPath();
+context.fillStyle = '#707cd5'
+context.fillRect(50, 500, 500, 100);
+context.fillRect(150, 400, 300, 100);
+context.fillRect(250, 300, 100, 100);
+context.fillRect(50, 300, 100, 100);
+context.fillRect(450, 300, 100, 100);
+context.fillRect(150, 200, 300, 100);
+context.fillRect(150, 100, 100, 100);
+context.fillRect(350, 100, 100, 100);
+context.stroke();
+}
